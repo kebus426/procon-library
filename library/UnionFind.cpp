a92a90af0b3c7d2c8ced04948a903ae588a64b44
kebus426 <kemu423@gmail.com>
2019-05-20 02:03:04 +0900

    UnionFind.hppからUnionFind.cppに実装を移動

--- library/UnionFind.cpp
+++ library/UnionFind.cpp
@@ -1,5 +1,55 @@
-#include "UnionFind.h"
-
 #include <vector>
 
+class UnionFind
+{
 
+private:
+	std::vector<long long> par;
+	std::vector<long long> rank;
+
+public:
+	void init(long long n) {
+		par = std::vector<long long>(n);
+		rank = std::vector<long long>(n);
+		for (long long i = 0; i < n; i++) {
+			par[i] = i;
+			rank[i] = 0;
+		}
+	};
+
+	long long find(long long x) {
+		if (par[x] == x) {
+			return x;
+		}
+		else {
+			return par[x] = find(par[x]);
+		}
+	}
+
+	void unite(long long x, long long y) {
+		x = find(x);
+		y = find(y);
+		if (x == y)return;
+
+		if (rank[x] < rank[y]) {
+			par[x] = y;
+		}
+		else {
+			par[y] = x;
+			if (rank[x] == rank[y])rank[x]++;
+		}
+
+	}
+
+	bool same(long long x, long long y) {
+		return find(x) == find(y);
+	}
+
+	long long count() {
+		long long ret = 0;
+		for (long long i = 0; i < par.size(); i++) {
+			if (i == par[i])ret++;
+		}
+		return ret;
+	}
+};
